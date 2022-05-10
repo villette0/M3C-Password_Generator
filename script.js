7b8f480c3d2253dcdd5b308cdf6d73c73bf30b90
violet978 <villette@live.com>
2022-05-10 16:39:16 -0700

    Added random character functions to javascript and acceptance criteria to readme.

--- script.js
+++ script.js
@@ -12,3 +12,38 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+//Alert box
+alert("Hello! I am an alert box!!");
+
+
+
+//Still working: Object of functions:
+var randomFunction = {
+  lowerLetter: getRandomLowercaseLetter,
+  upperLetter: getRandomUppercaseLetter,
+  number: getRandomNumber,
+  symbol: getRandomSymbol,
+};
+
+
+//Variable for symbol characters to use in getRandomSymbol function.
+var symbols = "!#$%&'()]*+,-./:;<=>?@[^_`{|}~"
+
+//Functions for producing each type of random character:
+function getRandomLowercaseLetter() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUppercaseLetter() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  return symbols[Math.floor(Math.random() * symbols.length)]
+}
