a01e7414da977bd5b6fd2b3c02c7a95978907166
violet978 <villette@live.com>
2022-05-10 21:10:59 -0700

    Added functions and comments.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var isNumberIncluded; 
+var isLowercaseIncluded;
+var isUppercaseIncluded;
+var isSymbolIncluded;  
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
 function writePassword() {
@@ -7,43 +14,65 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-  
-  function generatePassword() {
-    var numCharacters = prompt("How long would you like the password to be (8-126 characters)?") 
-    console.log(numCharacters);
-    if (numCharacters != null && numCharacters != '') {
-      randomFunction();
-    }
-    else if (numCharacters === null) {
-      return;
-    }
-    else  {
+}
+
+//Prompts user and generates password based on if "confirm questions" are true (okay) or false (cancelled)
+function generatePassword() {
+  var numOfCharacters = prompt("How long would you like the password to be (8-128 characters)?"); 
+
+  if (numOfCharacters != null && numOfCharacters != '' && (numOfCharacters > 7) && (numOfCharacters < 129)) {
+    isNumberIncluded = confirm("Would you like the password to contain numbers?");
+    isLowercaseIncluded = confirm("Would you like the password to contain lowercase letters?");
+    isUppercaseIncluded = confirm("Would you like the password to contain uppercase letters?");
+    isSymbolIncluded = confirm("Would you like the password to contain symbols?");
+    
+    if (!isNumberIncluded && !isLowercaseIncluded && !isUppercaseIncluded && !isSymbolIncluded) {
+      alert("You must choose at least one character type");
       generatePassword();
     }
+    return getRandomPassword(numOfCharacters);
+  }
+  else if (numOfCharacters === null) { //clicking cancel produces null
+    return;
+  }
+  else  {
+    generatePassword(); //reprompts you if you don't enter anything
   }
 }
 
-//Overall function
-function randomFunction() {
-  getRandomLowercaseLetter();
-  getRandomUppercaseLetter();
-  getRandomNumber();
-  getRandomSymbol();
-  console.log(getRandomSymbol());
+//Overall function for password creation
+function getRandomPassword(numOfCharacters) {
+  var newPassword = "";
+  for (var i = 0; i < numOfCharacters; i++) {
+    var newCharacter = getRandomCharacter();
+    newPassword = newPassword.concat(newCharacter); //Adds a new character to newPassword as many times as is the length of the chosen password; e.g. 8 characters long.
+  }
+  return newPassword;
 }
 
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-//My additions, still working: 
-//confirm("Would you like the password to contain number?")
-
-//Variable for symbol characters to use in getRandomSymbol function.
-var symbols = "!#$%&'()]*+,-./:;<=>?@[^_`{|}~"
+//Checking if each question is true, and pushing to a randomized, combined array
+function getRandomCharacter() {
+  var characterArray = [];
+  if (isNumberIncluded) {
+    characterArray.push(getRandomNumber());
+  }
+  if (isLowercaseIncluded) {
+    characterArray.push(getRandomLowercaseLetter());
+  }
+  if (isUppercaseIncluded) {
+    characterArray.push(getRandomUppercaseLetter());
+  }
+  if (isSymbolIncluded) {
+    characterArray.push(getRandomSymbol());
+  }
+  return characterArray[Math.floor(Math.random() * characterArray.length)]
+} 
 
 //Functions for producing each type of random character:
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
 function getRandomLowercaseLetter() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
 }
@@ -52,10 +81,7 @@ function getRandomUppercaseLetter() {
   return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
 }
 
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
+var symbols = "!#$%&'()]*+,-./:;<=>?@[^_`{|}~" 
 function getRandomSymbol() {
   return symbols[Math.floor(Math.random() * symbols.length)]
 }
